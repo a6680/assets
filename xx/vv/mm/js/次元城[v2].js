2720a76573247d76f910e1031719c9c33d91783f
a6680 <a1762416786@gmail.com>
2024-01-28 15:54:55 +0800

    update

---
+++ xx/vv/mm/js/次元城[v2].js
@@ -0,0 +1,24 @@
+var rule = {
+	title: '次元城[V2]', // csp_AppYsV2
+	host: 'http://app.95189371.cn',
+	homeUrl:'/ciyuancheng.php/v6/index_video?token=&csrf=ca/dm6kbSsNU6Ql0I+Pizn2B+hl1wN9yaKEBxcs2RISNAb5tmVQiqmobdxkvB3wnxu0uqhcoOhaONma9RsDmeAmketUdjc4gZ848vK4JwLo+ij81RPd27FxEGDwdAAfjwNyjJISiLogwYY6L21ZIJZIvz2aAOVUYaZMtzEwslMY=',
+	url: '/ciyuancheng.php/v6/video?pg=fypage&tid=fyclass&class=&area=&lang=&year=&order=time&token=&csrf=BqA34o9i1dZx7DUt7lMMYzTw60YS1BJGrFD8kbLpKFL6kiizOcoiUYau9lyYlQqC7m4kprq+VqVUGuaTpnRlbPSY/3a5ckMKuIYy1B30bhZPBxsS8rJr/zdrdBhYKv0xwtjSb988nhg2aghHGMxTTNifDVyNC0HSLX4vaV79SkQ=',
+	detailUrl:'/ciyuancheng.php/v6/video_detail?id=fyid&ck=VjDvMJmo+vRpUbrPkuPypJSo0QKGXSH/WLHNHe2dc5R03/DZMV9BAj0YrltfLNRKYkUb0jp+UKVDhwIn1zdsak4tmx5MR1y2d9tmsujpE2/nieazQwZ1OZHmADnTXSiOXtjwzz5ouv1dqcdokT4Ye1uxtOLjtMKX56wJDs0cUwYqyc7DLmpCaTWop7SAflvV7bw7YsmgD1Mx8wU1FBcsg9RobD0jP3YwBgB5QF4opVvWwGNYUx2vysHWuc3uboIQWsVrWt2o70lb5GSABc6TY5K0FXYMkvEsPWo505jewjseyt2N1pRB+8ZAkBlMByix05ltMgiJogHgts9C6rnfyJ48yi+pg3KtdQ7ATYvheFy0A0kI9qvaQ+eFOBpQ/XKs/eoF6BB4mRiIi4O2cGedv54wJS7PwmeF1Zz/MHXNRoeIj17i9Hmpq/MpOYe275QMhwv+FqTYE5ybWd8dXrnr4rYi0IeKlYf5zowsQz9ZiCJZXPj/9/OswCb3lqHH6yeoduJ001Xzi5SXEHgQR6QYxEz728PCrj2ux4jTkMCTcaz8Qt2HOZoSSX/r0kKmxZ1F6cqsyME4+QdFNBNK/jnKQbzDDyY0hYeP4rFQU8wuzr2mGIEHMBKVk4zcVV54zx+sQRclM9p/1E9sOrcb5kGxPHWsuScijRobOPfjiW1lsJ1yJ5TvjOXtvYsHciunvtColaEKKRc3Xbg0/6jD4GckeAdtAyY9UUWQdyeZ1m8W0B/ixfA57fyUczB2m4YXAm0/kUVomAY/jgQAAUh5yqngaPCyNlFqQl4nuN91Wm83bu6TZjm7kLUrrsQwE7yWIgX5k2/ZJbRShH0FiX+RL+YowXdSG28uqT+0kAuiYcg10GeaHUardsM36ObVhoZMAUvsj3rsOmGXTLxNLA9/YKErk6J9ChWGCvsgh8uCDwVHf/K5kjywdv4vA/803fXGaMk/SO4uqLdKbBkGDYi0tXCR0mq6MtunjlSL4edoqDOhbCwZWUIBWM4/HCLIIgJXnD7WPffnBoO4hLF2656e08hd4dzSbpq16g1Bl1uQYsY/YUh3JGve89EiGA0YOEcNHbwYAP35BVFYMjuezEETZrS1Y2D5u5dnSMpRs+DgjoMfDSrqD51GnqR1R2d+S4lCkVnGtJzEIDE/F7kfFLXzK58tPbMyaVqq1ZNjKcyMlYt2/a7R+L+JY2r37/KuRgePYcD+v6S9R8/v44xmQ3IT3baI4dDWSOUQ57Isy+MnPJTFIOQIIazAHWYRAMvXrxOPfJXZ8c/fX8YCWsgGVncvINFrQDF/VsjraVN6+OojW+aNUe3Nz1qQBobm2QjEWHZLPKIkiG7i8we5HfX2uSTU+ePJU9bdgb+zMWqHgJWhcK5uYMRUZb+9GH+cFRPC/CB8EnWItdBZEMsel5/nwTgobAFu4Cy1fjqcrF1iM2SxdyUbCSpLSh+Qr/C7qUPoFXBUagAcZCXCKOt0BtOm9O3+7HhE7wiesZuAHwuJrqHO5Qy081NkvUUJtq+xLE3QhwCeX9Uc&pkid=com.jarworld.bleach.bvn.sjm.cyc&token=&csrf=u04d704aO6Cz80EfimGkJX8hDwsxK5xk5iiUJMvkGpQg+a4iOcserPOk/K8HHEZGXV2N5eloPJopoMLFrNEkudwYlK2mlOHy0DZHRg1BBIOJsMTT6VclH0Wh2hx49e8mWE2k/38yqrTFAHe+KCuS9RdAe3yzBta3VRvTe9F/ZdE=',
+	searchUrl: '/ciyuancheng.php/v6/search?pg=fypage&tid=0&text=**&token=&csrf=Sdwrng+adcvD4nWetcSMhEg1mcwNYhi9K+RXCBxEpMSxIpjOVarR7K+Ev3yCtdxeAm/qs0Ck4TgJcRtcc955KFqfh6I454OCM9PQtkYnP+eC4UOT0IWdvsqRgNLaMgePwaOkDB+djFy43eVOkVB3vp0r/jPxErHa9xAZQiR0Sio=',
+	headers:{'User-Agent':'Dart/2.18 (dart:io)'},
+	timeout:5000,
+	class_name:'番剧&剧场&4K', // 分类筛选 /xgapp.php/v1/nav
+	class_url:'20&21&26',
+	play_parse:true,
+//	lazy:'js:input={parse:1,jx:1,url:input};',
+//	lazy:'js:input={parse:1,url:input};',
+//	lazy:'js:input="https://json.95189371.cn/zijian/json.php?url="+input',
+//  lazy:'js:input="https://json.95189371.cn/zijian/json.php?url="+input.split("?")[0];log(input);let html=JSON.parse(request(input));log(html);input=html.url||input',
+	lazy:'js:input=/video|.mp3|.m3u8/.test(input)?JSON.parse(request("https://json.95189371.cn/zijian/json.php?url="+input)).url:input',
+
+	limit:6,
+	推荐:'json:data[0].vlist;*;*;*;*',
+	一级:'json:data;vod_name;vod_pic;vod_remarks;vod_id',
+	二级:'js:try{let html=request(input);print(html);html=JSON.parse(html);let node=html.data.vod_info;VOD={vod_id:node["vod_id"],vod_name:node["vod_name"],vod_pic:node["vod_pic"],type_name:node["vod_class"],vod_year:node["vod_year"],vod_area:node["vod_area"],vod_remarks:node["vod_remarks"],vod_actor:node["vod_actor"],vod_director:node["vod_director"],vod_content:node["vod_content"].strip()};let episodes=node.vod_url_with_player;let playMap={};if(typeof play_url==="undefined"){var play_url=""}episodes.forEach(function(ep){let source=ep["name"];if(!playMap.hasOwnProperty(source)){playMap[source]=[]}playMap[source].append(ep["url"])});let playFrom=[];let playList=[];Object.keys(playMap).forEach(function(key){playFrom.append(key);playList.append(playMap[key])});let vod_play_from=playFrom.join("$$$");let vod_play_url=playList.join("$$$");VOD["vod_play_from"]=vod_play_from;VOD["vod_play_url"]=vod_play_url}catch(e){log("获取二级详情页发生错误:"+e.message)}',
+	搜索:'*',
+}
